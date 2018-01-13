ddc4504522bea6a66276e39b575f515f73f35cec
wywywy0823 <32708951+wywywy0823@users.noreply.github.com>
2018-01-13 11:29:43 +0800

    hhh

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 /*
 Theme: Flatfy Theme
 Author: Andrea Galanti
-Bootstrap Version 
+Bootstrap Version
 Build: 1.0
 
 */
 
-$(window).load(function() { 
-	//Preloader 
-	$('#status').delay(300).fadeOut(); 
+$(window).load(function() {
+	//Preloader
+	$('#status').delay(300).fadeOut();
 	$('#preloader').delay(300).fadeOut('slow');
 	$('body').delay(550).css({'overflow':'visible'});
 })
@@ -18,19 +18,19 @@ $(document).ready(function() {
 		$(".navbar-brand").hover(function () {
 			$(this).toggleClass("animated shake");
 		});
-		
+
 		//animated scroll_arrow
 		$(".img_scroll").hover(function () {
 			$(this).toggleClass("animated infinite bounce");
 		});
-		
+
 		//Wow Animation DISABLE FOR ANIMATION MOBILE/TABLET
 		wow = new WOW(
 		{
 			mobile: false
 		});
 		wow.init();
-		
+
 		//MagnificPopup
 		$('.image-link').magnificPopup({type:'image'});
 
@@ -51,6 +51,13 @@ $(document).ready(function() {
 			  singleItem:true
 		});
 
+		$("#owl-demo-2").owlCarousel({
+			  navigation : false, // Show next and prev buttons
+			  slideSpeed : 300,
+			  paginationSpeed : 400,
+			  singleItem:true
+		});
+
 		//SmothScroll
 		$('a[href*=#]').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -64,13 +71,12 @@ $(document).ready(function() {
 					}
 			}
 		});
-		
+
 		//Subscribe
 		new UIMorphingButton( document.querySelector( '.morph-button' ) );
 		// for demo purposes only
-		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
+		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) {
 			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
 		} );
 
 });
-
